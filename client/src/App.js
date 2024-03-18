ffeb022e700ec9e90676d5f98e11c088e46a3557
Your Name <miroua132@gmail.com>
2024-03-18 04:19:30 +0100

    upload

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ useEffect(()=>{
 
 if(list===null){
 
-  axios.get("http://localhost:3001/getList").then((result)=>{
+  axios.get("https://password-manager-server-cyan.vercel.app/getList").then((result)=>{
 
   setList(result.data)
   
@@ -30,7 +30,7 @@ if(list===null){
 <div className='bg-blue-200 flex flex-col w-3/4 items-center gap-5 pt-10 pb-10 mt-10 h-2/4'>
 <input className='w-3/4 h-10 rounded-lg' type="text" placeholder='Ex. password123' onChange={(e)=>{setPassword(e.target.value)}} />
 <input className='w-3/4 h-10 rounded-lg' type="text" placeholder='Ex. LinkedIn'  onChange={(e)=>{setForAccount(e.target.value)}}/>
-<button className='bg-slate-200 rounded-lg w-3/4 h-10' onClick={()=>{axios.post("http://localhost:3001/register",{name : account,password : password}).then((result)=>{
+<button className='bg-slate-200 rounded-lg w-3/4 h-10' onClick={()=>{axios.post("https://password-manager-server-cyan.vercel.app/register",{name : account,password : password}).then((result)=>{
 
 })}}>save</button>
 </div>
@@ -40,7 +40,7 @@ if(list===null){
 <div className='w-3/4' key={i}><button className='bg-black text-white w-full h-10 rounded-lg' onClick={()=>{
 
 
-axios.post("http://localhost:3001/get",{name : e.name}).then((result)=>{
+axios.post("https://password-manager-server-cyan.vercel.app/get",{name : e.name}).then((result)=>{
   alert(e.name+" password is : "+ result.data)
 
 
